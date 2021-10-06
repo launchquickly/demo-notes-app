09fdbe7176709bd91a3f48e657ac0bf57656ebce
pauljkearney <hello@launchquickly.scot>
2021-10-06 22:11:01 +0100

    Signup with AWS Cognito

--- frontend/src/containers/Signup.js
+++ frontend/src/containers/Signup.js
@@ -1,7 +1,9 @@
+import { Auth } from "aws-amplify";
 import Form from "react-bootstrap/Form";
 import LoaderButton from "../components/LoaderButton";
 import React, { useState } from "react";
 
+import { onError } from "../lib/errorLib";
 import { useFormFields } from "../lib/hooksLib";
 
 import "./Signup.css";
@@ -35,15 +37,34 @@ export default function Signup() {
 
     setIsLoading(true);
 
-    setNewUser("test");
-
-    setIsLoading(false);
+    try {
+        const newUser = await Auth.signUp({
+            username: fields.email,
+            password: fields.password,
+        });
+        setIsLoading(false);
+        setNewUser(newUser);
+    } catch(e) {
+        onError(e);
+        setIsLoading(false);
+    }
   }
 
   async function handleConfirmationSubmit(event) {
     event.preventDefault();
 
     setIsLoading(true);
+
+    try {
+        await Auth.confirmSignUp(fields.email, fields.confirmationCode);
+        await Auth.signIn(fields.email, fields.password);
+    
+        userHasAuthenticated(true);
+        history.push("/");
+    } catch (e) {
+        onError(e);
+        setIsLoading(false);
+    }
   }
 
   function renderConfirmationForm() {
